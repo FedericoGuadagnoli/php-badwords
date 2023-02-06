2c64fe2d9ad4abf28d622993dc9d994fa534ba98
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-06 13:56:27 +0100

    create method strlen & add style

--- badword.php
+++ badword.php
@@ -1,12 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad-Words</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    
+</body>
+</html>
+
+
 <?php
 $text = $_GET['text'];
-$badword= $_GET['badword'];
+$badword = $_GET['badword'];
+$lengText = 'La lunghezza del testo è ';
 
 $replaced_badword = str_replace($badword, '***', $text);
 ?>
 
-<h1>Paragrafo originale</h1>
-<p><?php echo $text ?></p>
+<div>
+    <h1>Paragrafo originale</h1>
+    <p><?php echo $text?></p>
+    <span><?php echo $lengText . strlen($text)?></span>
+</div>
 
-<h1>Paragrafo censurato</h1>
-<p><?php echo $replaced_badword?></p>
+<hr>
+
+<div>
+    <h1>Paragrafo censurato</h1>
+    <p><?php echo $replaced_badword?></p>
+    <span><?php echo $lengText . strlen($replaced_badword)?></span>
+</div>
+<hr>
